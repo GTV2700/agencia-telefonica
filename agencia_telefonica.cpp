c092df032bf5fb2278d6f12c918f00add4a5e328
Gustavo Herrera <gustavoamnaturalesa@gmail.com>
2026-01-05 15:23:12 -0400

    configuracion inicial

--- agencia_telefonica.cpp
+++ agencia_telefonica.cpp
@@ -1,4 +1,8 @@
 #include<stdio.h>
+//integrantes:
+//Gustavo Herrera 32.788.274
+//Emil Gomez 31.695.619
+//Daniel Daza 31.129.232
 //objetivos:
 //1.Registro de contactos con telefono y nombre
 //2.buscar contacto por inicial del nombre
@@ -7,6 +11,42 @@
 //5.usar recursion para buscar coincidencias
 int main(){
     printf("Agencia Telefonica De Busqueda Inteligente");
-
-    return 0;
+    int op = 2;
+    int selec;
+while (op != 0)
+{
+    printf("seleciones una opcion:");
+    printf("1.reistro de contacto");
+    printf("2.busqueda de contacto");
+    printf("3.editar y cargar contacto");
+    printf("4.guardar y cargar desde archivo");
+    printf("5.salir");
+    scanf("%d",&selec);
+switch (selec)
+{
+ //Regristro de contacto
+    case 1:
+     printf("h1");
+        break;
+    //Buscar contacto por inicial    
+    case 2:
+    printf("h2");
+        break;
+    //editar contacto y cargar contacto     
+    case 3:
+    printf("h3");
+        break;
+    //guardar y cargar desde archivo    
+    case 4:
+    printf("h4");
+        break;
+        //salir
+    case 5:
+    printf("exit");
+    op = 0;
+     break;
+}
+}
+printf("finish");
+return 0;
 }
