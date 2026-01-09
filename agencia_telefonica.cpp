dfa013c2aa1a30b78e7fe88b091c8e67ae4716ff
Gustavo Herrera <gustavoamnaturalesa@gmail.com>
2026-01-09 19:27:03 -0400

    feat:agrega estructura Info para datos de contacto y array

--- agencia_telefonica.cpp
+++ agencia_telefonica.cpp
@@ -1,32 +1,49 @@
 #include<stdio.h>
+#include <string.h>
 //integrantes:
 //Gustavo Herrera 32.788.274
 //Emil Gomez 31.695.619
 //Daniel Daza 31.129.232
+//Seccion D3
 //objetivos:
 //1.Registro de contactos con telefono y nombre
 //2.buscar contacto por inicial del nombre
 //3.editar y cargar contacto
 //4.guardar y cargar desde archivo
 //5.usar recursion para buscar coincidencias
+
+
+typedef struct Info
+{
+	char nombre[30];
+	char apellido[30];
+	char telefono[13];
+	char apodo[20];
+}info; //organiza las variable para solo tener que llamar la funion
+
+
+Info contactos[100];  //Array, registro de contactos
+int total_contactos = 0;
+
+
+
 int main(){
     printf("Agencia Telefonica De Busqueda Inteligente");
-    int op = 2;
-    int selec;
-while (op != 0)
+    int op;
+do
 {
-    printf("seleciones una opcion:");
-    printf("1.reistro de contacto");
-    printf("2.busqueda de contacto");
-    printf("3.editar y cargar contacto");
-    printf("4.guardar y cargar desde archivo");
-    printf("5.salir");
-    scanf("%d",&selec);
-switch (selec)
+    printf("seleciones una opcion.\n");
+    printf("1.reistro de contacto:\n");
+    printf("2.busqueda de contacto:\n");
+    printf("3.editar y cargar contacto:\n");
+    printf("4.guardar y cargar desde archivo:\n");
+    printf("5.salir:\n");
+    scanf("%d",&op);
+switch (op)
 {
- //Regristro de contacto
+ //Registro de contactos con telefono y nombre
     case 1:
-     printf("h1");
+    printf("h1");
         break;
     //Buscar contacto por inicial    
     case 2:
@@ -46,7 +63,8 @@ switch (selec)
     op = 0;
      break;
 }
-}
+}while (op != 0);
+
 printf("finish");
 return 0;
 }
